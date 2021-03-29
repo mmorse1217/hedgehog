db49c977ee57ef5863eebf68e1c26086d051a811
Matt Morse <mmorse1217@gmail.com>
2021-03-29 11:33:54 -0400

    updated PETSc to 3.14.5, regression tests are passing.

--- cmake/PETSc-configure.cmake
+++ cmake/PETSc-configure.cmake
@@ -4,10 +4,10 @@ include(ExternalProject)
     # Compile PETSc 
     ExternalProject_Add(
         PETSc
-        URL http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.7.2.tar.gz
+        URL http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-3.14.5.tar.gz
         PREFIX ${CMAKE_BINARY_DIR}/_deps/PETSc
         INSTALL_DIR ${CMAKE_BINARY_DIR}/_deps/PETSc
-        CONFIGURE_COMMAND cd <SOURCE_DIR> && ./configure --with-pic=true --with-shared-libraries=0 --prefix=<INSTALL_DIR> --with-debugging=0 --with-64-bit-indices=1 --with-single-library=1 
+        CONFIGURE_COMMAND cd <SOURCE_DIR> && ./configure --with-pic=0 --with-shared-libraries=0 --prefix=<INSTALL_DIR> --with-debugging=0 --with-64-bit-indices=1 --with-single-library=1 
         BUILD_COMMAND make -C <SOURCE_DIR>
         INSTALL_COMMAND make -C <SOURCE_DIR> install
     )
